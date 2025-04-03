1605a99c54ed3196f27fc19ba1ce4ca7e779d6b7
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-04-03 17:20:31 -0400

    Changes from 8.2

--- SnacktacularUI/SnacktacularUIApp.swift
+++ SnacktacularUI/SnacktacularUIApp.swift
@@ -22,7 +22,7 @@ struct SnacktacularUIApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
